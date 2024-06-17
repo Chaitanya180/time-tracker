12c9d7dccaa33acd85881ea168af39578b9d6289
Chaitanya180 <122300014+Chaitanya180@users.noreply.github.com>
2024-06-17 13:14:29 +0530

    Update TrackerCoreConfig.java

--- core/src/main/java/training/taylor/timetracker/core/TrackerCoreConfig.java
+++ core/src/main/java/training/taylor/timetracker/core/TrackerCoreConfig.java
@@ -3,7 +3,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import training.taylor.timetracker.core.dao.TimeEntry;
-
 import java.util.ArrayList;
 import java.util.List;
 
